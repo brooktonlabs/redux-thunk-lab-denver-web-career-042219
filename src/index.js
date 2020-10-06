5f70e87638ddfd60b3e72c32bcdb1655477ab7f9
David Brookton <david.zimmerman@colorado.edu>
2020-10-06 18:25:23 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import catsReducer from './reducers/catsReducer.js';
+
+const store = createStore(catsReducer)
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.getElementById('root')
 );
