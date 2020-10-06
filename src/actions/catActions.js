c15685fc62e4f45a39a9968a7e0e1686dc8d0e5e
David Brookton <david.zimmerman@colorado.edu>
2020-10-06 18:28:22 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,9 @@
 // write and export your action creator function here
+fetchCats = () => {
+  fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+    .then(response => {return response.json()})
+    .then(responseJSON => {
+  // instead of logging here, call dispatch and send the cat JSON data to your store
+      console.log(responseJSON.images)
+    })
+}
